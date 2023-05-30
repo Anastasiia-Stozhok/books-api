12bad3675b1dd7846bb7777ae77184e189b030bb
Anuta21 <anuta.200214@gmail.com>
2023-05-30 04:31:07 +0300

    readme

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,105 +1,108 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-import UserModel from '../models/User.js';
+import UserModel from "../models/User.js";
 
 export const register = async (req, res) => {
-    try {
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-        const doc = new UserModel({
-            name: req.body.name,
-            email: req.body.email,
-            passwordHash: hash,
-        });
+    const doc = new UserModel({
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: hash,
+    });
 
-        const user = await doc.save();
+    const user = await doc.save();
 
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret1608',
-            {
-                expiresIn: '30d',
-            },
-        );
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret5",
+      {
+        expiresIn: "30d",
+      }
+    );
 
-        const { passwordHash, ...userData } = user._doc
+    const { passwordHash, ...userData } = user._doc;
 
-        res.json({
-            ...userData,
-            token,
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Failed to register',
-        })
-    }
-}
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to register",
+    });
+  }
+};
 
 export const login = async (req, res) => {
-    try {
-        const user = await UserModel.findOne({ email: req.body.email })
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
 
-        if (!user) {
-            return res.status(404).json({
-                message: 'Incorrect login or password',
-            });
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-
-        if (!isValidPass) {
-            return res.status(400).json({
-                message: 'Incorrect login or password',
-            });
-        }
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d',
-            },
-        );
-
-        const { passwordHash, ...userData } = user._doc
-
-        res.json({
-            ...userData,
-            token,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Failed to login',
-        });
+    if (!user) {
+      return res.status(404).json({
+        message: "Incorrect login or password",
+      });
     }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Incorrect login or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret5",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to login",
+    });
+  }
 };
 
 export const getUser = async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.userId)
+  try {
+    const user = await UserModel.findById(req.userId);
 
-        if (!user) {
-            return res.status(404).json({
-                message: 'User not found',
-            });
-        }
-
-        const { passwordHash, ...userData } = user._doc
-
-        res.json(userData)
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'No access',
-        })
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
     }
-}
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "No access",
+    });
+  }
+};
--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,22 +1,22 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
-    const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
 
-    if (token) {
-        try {
-            const decoded = jwt.verify(token, 'secret123');
-            req.userId = decoded._id;
-            next();
-        } catch (e) {
-            console.log(e)
-            return res.status(403).json({
-                message: 'No Access',
-            });
-        }
-    } else {
-        return res.status(403).json({
-            message: 'No Access',
-        });
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, "secret5");
+      req.userId = decoded._id;
+      next();
+    } catch (e) {
+      console.log(e);
+      return res.status(403).json({
+        message: "No Access",
+      });
     }
+  } else {
+    return res.status(403).json({
+      message: "No Access",
+    });
+  }
 };
